96e8e3f8baaede6124212cfdc028161519f99d91
Nacho Mata <nachomataalmagro@gmail.com>
2024-07-13 23:35:12 +0200

    Added plugwise logic

--- Smart Gym Server/backend/mqtt_client.py
+++ Smart Gym Server/backend/mqtt_client.py
@@ -5,31 +5,58 @@ from datetime import datetime
 
 MQTT_BROKER = "localhost"
 MQTT_PORT = 1883
-MQTT_TOPIC = "workout"
+MQTT_WORKOUT_TOPIC = "workout"
+MQTT_PLUGWISE_TOPIC = "plugwise"
+MQTT_MACHINE_TOPIC = "machine"
+
+class PLUGWISE_STATUS:
+    ON = "on"
+    OFF = "off"
+class MACHINE_STATUS:
+    BUSY = "busy"
+    FREE = "free"
 
 db = DBHandler()
 
-def on_connect(client, userdata, flags, rc):
-    print(f"Connected with result code {rc}")
-    client.subscribe(MQTT_TOPIC)
+machines_status = {}
 
-def on_message(client, userdata, msg):
+def on_connect(client: mqtt.Client, userdata, flags, rc):
+    print(f"Connected with result code {rc}")
+    client.subscribe([(MQTT_WORKOUT_TOPIC, 0), (MQTT_MACHINE_TOPIC, 0)])
+
+def on_message(client: mqtt.Client, userdata, msg : mqtt.MQTTMessage):
     try:
         payload = msg.payload.decode('utf-8')
         data = json.loads(payload)
+        
+        print(f"Received message: {data}")
 
-        user_id = data.get('user_id')
-        machine_id = data.get('machine_id')
-        repetitions = data.get('repetitions')
-        weight = data.get('weight')
-        duration = data.get('duration')
-        date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        if msg.topic == MQTT_WORKOUT_TOPIC:
+            user_id = data.get('user_id')
+            machine_id = data.get('machine_id')
+            repetitions = data.get('repetitions')
+            weight = data.get('weight')
+            duration = data.get('duration')
+            date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-        if user_id and machine_id and repetitions:
-            db.add_workout(user_id, machine_id, date, repetitions, weight, duration)
-            print(f"Workout added: {data}")
-        else:
-            print("Incomplete data in the received message")
+            if user_id and machine_id and repetitions:
+                db.add_workout(user_id, machine_id, date, repetitions, weight, duration)
+                print(f"Workout added: {data}")
+            else:
+                print("Incomplete data in the received message")
+                
+        elif msg.topic == MQTT_MACHINE_TOPIC:
+            machine_id = data.get('machine_id')
+            status = data.get('status')
+            
+            global machines_status
+            machines_status[machine_id] = status
+            if status == MACHINE_STATUS.BUSY:
+                client.publish(MQTT_PLUGWISE_TOPIC, json.dumps({"machine_id": machine_id, "action": PLUGWISE_STATUS.ON}))
+            else:
+                client.publish(MQTT_PLUGWISE_TOPIC, json.dumps({"machine_id": machine_id, "action": PLUGWISE_STATUS.OFF}))
+
+
     except Exception as e:
         print(f"Error processing the message: {e}")
 
